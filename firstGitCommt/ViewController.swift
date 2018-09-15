314bae4ce5566b0863d8e15ab5949e6a0c1ca776
Deepan <deepan@deepans-mac-mini.local>
2018-09-15 17:06:34 +0530

    Initial Commit

---
+++ firstGitCommt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstGitCommt
+//
+//  Created by Deepan on 9/15/18.
+//  Copyright © 2018 Deepan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
